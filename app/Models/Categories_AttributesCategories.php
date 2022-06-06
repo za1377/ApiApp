3ccafc970503e5fa3e1dc6b476c25ad4c9f10bb5
za1377 <zahraanvari216@gmail.com>
2022-06-06 08:25:57 +0430

    add attributes method to Categories_AttributesCategories model

--- app/Models/Categories_AttributesCategories.php
+++ app/Models/Categories_AttributesCategories.php
@@ -18,4 +18,14 @@ class Categories_AttributesCategories extends Model
         'attre_cate_id',
         'category_id',
     ];
+
+    /**
+     * The attributes that belong to the Categories_AttributesCategories
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function attributes(): BelongsToMany
+    {
+        return $this->belongsToMany(Attributes::class, 'c_a_c_a', 'cate_atrre_cate_id', 'attributes_id');
+    }
 }
