121d58d7f23117ca1808bfd2779a86f88e1369b4
za1377 <zahraanvari216@gmail.com>
2022-06-13 09:26:36 +0430

    frist test for swagger

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,6 +20,53 @@ class HomeController extends Controller
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
+     * Register new user
+     *
+     * @param  Request  $request
+     * @return JsonResponse
+     *
+     * @OA\Post(
+     *     path="/users",
+     *     summary="Adds a new user - with oneOf examples",
+     *     description="Adds a new user",
+     *     operationId="addUser",
+     *     tags={"user"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone",
+     *                     oneOf={
+     *                     	   @OA\Schema(type="string"),
+     *                     	   @OA\Schema(type="integer"),
+     *                     }
+     *                 ),
+     *                 example={"id": "a3fb6", "name": "Jessica Smith", "phone": 12345678}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                     property="id",
+     *                     type="string"
+*                  ),
+     *             @OA\Examples(example="result", value={"success": true}, summary="An result object."),
+     *             @OA\Examples(example="bool", value=false, summary="A boolean value."),
+     *         )
+     *     )
+     * )
      */
     public function index()
     {
