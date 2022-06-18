4ddd607a4c70b4f1c64bd1a2281aacc1f4a77f80
za1377 <zahraanvari216@gmail.com>
2022-06-18 08:25:01 +0430

    add delete route for brands

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SanctumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->get('/home', 'App\Http\Controllers\SanctumController@home');
+// Route::post('/login',function () {
+//     return response()->json(["message" => "login route"], 200);
+// });
+
+Route::post('/login', 'App\Http\Controllers\SanctumController@logIn');
+
+Route::post('/NewBrands', 'App\Http\Controllers\BrandsController@insert');
+Route::put('/UpdateBrands', 'App\Http\Controllers\BrandsController@update');
+Route::delete('/DeleteBrands', 'App\Http\Controllers\BrandsController@delete');
+
+
+// Route::middleware('Ensure:sanctum')->get('/', function () {
+//     return response()->json(['name' => "home"]);
+// });
