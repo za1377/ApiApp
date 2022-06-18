139632505c528218e08467c5fb87e4f0b92c85db
za1377 <zahraanvari216@gmail.com>
2022-06-18 08:23:21 +0430

    add delete methods to the controller

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -26,7 +26,7 @@ class BrandsController extends Controller
      *    @OA\JsonContent(
      *       required={"name","slug"},
      *       @OA\Property(property="name", type="string", format="name", example="sony"),
-     *       @OA\Property(property="slug", type="string", format="slug", example="/sony"),
+     *       @OA\Property(property="slug", type="string", format="slug"),
      *    ),
      * ),
      * @OA\Response(
@@ -38,12 +38,12 @@ class BrandsController extends Controller
      *     )
      * ),
      * @OA\Response(
-     *         response=201,
+     *         response=200,
      *         description="OK",
      *         @OA\JsonContent(
      *             @OA\Property(
-     *                     property="id",
-     *                     type="biginteger"
+     *                     property="message",
+     *                     type="string"
      *                  ),
      *         )
      *     )
@@ -86,7 +86,7 @@ class BrandsController extends Controller
      *     )
      * ),
      * @OA\Response(
-     *         response=200,
+     *         response=201,
      *         description="OK",
      *         @OA\JsonContent(
      *             @OA\Property(
@@ -94,7 +94,7 @@ class BrandsController extends Controller
      *                     type="string"
      *                  ),
      *         )
-     *     )
+     *     ),
      *
      */
     public function update(Request  $request){
@@ -103,5 +103,50 @@ class BrandsController extends Controller
                     "name" => $request->name,
                     "slug" => $request->slug]);
         return response()->json($brand, 200);
+    }
+
+        /**
+     * update the specefic data of brands table
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return response
+     *
+     * @OA\Delete(
+     * path="/DeleteBrands",
+     * summary="delete the row of the brands that is determined",
+     * description="update name and slug by admin",
+     * tags={"brands"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="delete brands name and slug",
+     *    @OA\JsonContent(
+     *       required={"id"},
+     *       @OA\Property(property="id", type="string", format="id", example="1"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
+     *        )
+     *     )
+     * ),
+     *  @OA\Response(
+     *         response=202,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                     property="message",
+     *                     type="string"
+     *                  ),
+     *         )
+     *     ),
+     *
+     */
+    public function delete(Request  $request){
+        $id = intval($request->id);
+        Brands::find($id)->delete();
+        return response()->json(["message" => "delete"], 200);
     }
 }
