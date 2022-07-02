b4b5ab97685e262ac602995b5cb391ebb21d9b29
za1377 <zahraanvari216@gmail.com>
2022-07-02 16:09:00 +0430

    add update method for AttributeCategories controller

--- app/Http/Controllers/AttributeCategoriesController.php
+++ app/Http/Controllers/AttributeCategoriesController.php
@@ -113,4 +113,86 @@ class AttributeCategoriesController extends Controller
         }
 
     }
+
+    /**
+     * update the specefic data of AttributeCategories table
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return response
+     *
+     * @OA\Put(
+     * path="/Update/Brands",
+     * summary="update the name of the AttributeCategories that use in categories",
+     * description="update name and slug by admin",
+     * tags={"brands"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass brands name and slug",
+     *    @OA\JsonContent(
+     *       required={"id","name","slug"},
+     *       @OA\Property(property="id", type="string", format="id", example="5"),
+     *       @OA\Property(property="name", type="string", format="name", example="weight"),
+     *       @OA\Property(property="slug", type="string", format="slug", example="/weight"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
+     *        )
+     *     )
+     * ),
+     * @OA\Response(
+     *    response=404,
+     *    description="Not_Found",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, your data not found.")
+     *        )
+     *     )
+     * ),
+     *
+     * @OA\Response(
+     *         response=202,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                     property="message",
+     *                     type="boolean"
+     *                  ),
+     *         )
+     *     ),
+     *
+     */
+    public function update(Request  $request){
+        $id = intval($request->id);
+        $data = array();
+
+        if($request->name != ""){
+            $data += ['name' => $request->name];
+        }
+        if($request->slug != ""){
+            $data += ['slug' => $request->slug];
+        }
+
+        $result = AttributeCategories::where('slug' , $request->slug)->get();
+        if($result->count() > 0){
+            return response()->json(['message' , 'These data can not be insert.'],409);
+        }
+
+        $query = AttributeCategories::find($id);
+
+        if(! is_null($query)){
+
+            if($data == []){
+                return response()->json(['message' , 'nothing for update.'],422);
+            }else{
+                $cate_attr = $query->update($data);
+                return response()->json($cate_attr, 200);
+            }
+
+        }else{
+            return response()->json(['message' => 'Sorry, your data not found.'] , 404);
+        }
+    }
 }
