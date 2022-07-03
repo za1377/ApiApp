2be4d8fb60b5772198c20e15ec2c213ce67142c2
za1377 <zahraanvari216@gmail.com>
2022-07-03 12:31:13 +0430

    add classes that is use in attributetype controller

--- app/Http/Controllers/AttributeTypeController.php
+++ app/Http/Controllers/AttributeTypeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AttributesTypes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Hash;
 
 class AttributeTypeController extends Controller
 {
