942eebf4bd6a46276e57f3af04ceb1003b5bac74
za1377 <zahraanvari216@gmail.com>
2022-07-03 11:29:55 +0430

    add classes that use in controller

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attributes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Hash;
 
 class AttributeController extends Controller
 {
