6782524d55619c7b603fc01d236d8e6df0ec9568
za1377 <zahraanvari216@gmail.com>
2022-07-04 12:32:03 +0430

    edit the error place of BrandsController

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Hash;
-
-
+use App\Http\Requests\BrandRequest;
+use App\Http\Resources\BrandCollection;
 
 class BrandsController extends Controller
 {
@@ -20,21 +20,19 @@ class BrandsController extends Controller
      * @return response
      *
      * @OA\Get(
-     * path="/Brands",
-     * summary="Show all of brands",
-     * description="display all of brands",
-     * tags={"brands"},
-
-     * @OA\Response(
-     *    response=404,
-     *    description="Wrong credentials response",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="Not found.")
-     *        )
-     *     )
-     * ),
+     *      path="/Brands",
+     *      summary="Show all of brands",
+     *      description="display all of brands",
+     *      tags={"brands"},
      *
-     * @OA\Response(
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string")
+     *          )
+     *      ),
+     *      @OA\Response(
      *         response=200,
      *         description="OK",
      *         @OA\JsonContent(
@@ -44,6 +42,8 @@ class BrandsController extends Controller
      *                  ),
      *         )
      *     )
+     * ),
+     *
      *
      */
     public function show(){
@@ -57,62 +57,53 @@ class BrandsController extends Controller
     /**
      * insert data to brands table
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\BrandRequest  $request
      * @return response
      *
      * @OA\Post(
-     * path="/Brand",
-     * summary="insert the name of the brands that use in categories",
-     * description="insert name and slug by admin",
-     * tags={"brands"},
-     * @OA\RequestBody(
-     *    required=true,
-     *    description="Pass brands name and slug",
-     *    @OA\JsonContent(
-     *       required={"name","slug"},
-     *       @OA\Property(property="name", type="string", format="name", example="sony"),
-     *       @OA\Property(property="slug", type="string", format="slug", example="/sony"),
-     *    ),
-     * ),
-     * @OA\Response(
-     *    response=422,
-     *    description="Wrong credentials response",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
-     *        )
-     *     )
-     * ),
+     *      path="/Brand",
+     *      summary="insert the name of the brands that use in categories",
+     *      description="insert name and slug by admin",
+     *      tags={"brands"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Pass brands name and slug",
+     *          @OA\JsonContent(
+     *              required={"name","slug"},
+     *              @OA\Property(property="name", type="string", format="name", example="sony"),
+     *              @OA\Property(property="slug", type="string", format="slug", example="/sony"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Wrong credentials response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
+     *          )
+     *      ),
      *
-     * @OA\Response(
-     *    response=409,
-     *    description="conflict",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="These data can not be insert.")
-     *        )
-     *     )
-     * ),
+     *      @OA\Response(
+     *          response=409,
+     *          description="conflict",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string")
+     *          )
+     *      ),
      *
-     * @OA\Response(
-     *         response=201,
-     *         description="OK",
-     *         @OA\JsonContent(
-     *             @OA\Property(
-     *                     property="message",
-     *                     type="string"
-     *                  ),
-     *         )
-     *     )
+     *      @OA\Response(
+     *          response=201,
+     *          description="created",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string")
+     *          )
+     *      ),
+     * ),
      *
      */
-    public function insert(Request  $request){
-        $validated = Validator::make($request->all(), [
-            'name' => 'required|string',
-            'slug' => 'required',
-        ]);
+    public function insert(BrandRequest  $request){
 
-        if ($validated->fails()) {
-            return response()->json(['message' , 'Sorry, your data not supported'],422);
-        }else{
+        $validated = $request->validated();
+
             $matchThese = ['slug' => $request->slug];
             $old_brand = Brands::where($matchThese)->get();
             if($old_brand->count() > 0){
@@ -121,10 +112,9 @@ class BrandsController extends Controller
                 $brand = Brands::create([
                     "name" => $request->name,
                     "slug" => $request->slug]);
-                return response()->json($brand, 200);
+                    $id = $brand->id;
+                    return response()->json($brand, 201);
             }
-
-        }
 
     }
 
@@ -135,47 +125,47 @@ class BrandsController extends Controller
      * @return response
      *
      * @OA\Put(
-     * path="/Brand",
-     * summary="update the name of the brands that use in categories",
-     * description="update name and slug by admin",
-     * tags={"brands"},
-     * @OA\RequestBody(
-     *    required=true,
-     *    description="Pass brands name and slug",
-     *    @OA\JsonContent(
-     *       required={"id","name","slug"},
-     *       @OA\Property(property="id", type="string", format="id", example="12"),
-     *       @OA\Property(property="name", type="string", format="name", example="sumsung"),
-     *       @OA\Property(property="slug", type="string", format="slug", example="/sumsung"),
-     *    ),
-     * ),
-     * @OA\Response(
-     *    response=422,
-     *    description="Wrong credentials response",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
-     *        )
-     *     )
-     * ),
-     * @OA\Response(
-     *    response=404,
-     *    description="Not_Found",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="Sorry, your data not found.")
-     *        )
-     *     )
-     * ),
+     *      path="/Brand",
+     *      summary="update the name of the brands that use in categories",
+     *      description="update name and slug by admin",
+     *      tags={"brands"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Pass brands name and slug",
+     *          @OA\JsonContent(
+     *              required={"id","name","slug"},
+     *              @OA\Property(property="id", type="string", format="id", example="12"),
+     *              @OA\Property(property="name", type="string", format="name", example="sumsung"),
+     *              @OA\Property(property="slug", type="string", format="slug", example="/sumsung"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not_Found",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Sorry, your data not found.")
+     *          )
+     *      ),
      *
-     * @OA\Response(
-     *         response=202,
-     *         description="OK",
+     *      @OA\Response(
+     *         response=200,
+     *         description="update",
      *         @OA\JsonContent(
      *             @OA\Property(
      *                     property="message",
      *                     type="boolean"
-     *                  ),
+     *             ),
      *         )
-     *     ),
+     *      ),
+     * ),
+     *
      *
      */
     public function update(Request  $request){
@@ -191,7 +181,7 @@ class BrandsController extends Controller
 
         $result = Brands::where('slug' , $request->slug)->get();
         if($result->count() > 0){
-            return response()->json(['message' , 'These data can not be insert.'],409);
+            return response()->json(['message' , 'These data can not be insert.'],400);
         }
 
         $query = Brands::find($id);
@@ -199,7 +189,7 @@ class BrandsController extends Controller
         if(! is_null($query)){
 
             if($data == []){
-                return response()->json(['message' , 'nothing for update.'],422);
+                return response()->json(['message' , 'nothing for update.'],400);
             }else{
                 $brand = $query->update($data);
                 return response()->json($brand, 200);
@@ -217,36 +207,37 @@ class BrandsController extends Controller
      * @return response
      *
      * @OA\Delete(
-     * path="/Brand",
-     * summary="delete the row of the brands that is determined",
-     * description="update name and slug by admin",
-     * tags={"brands"},
-     * @OA\RequestBody(
-     *    required=true,
-     *    description="delete brands name and slug",
-     *    @OA\JsonContent(
-     *       required={"id"},
-     *       @OA\Property(property="id", type="string", format="id", example="1"),
-     *    ),
-     * ),
-     * @OA\Response(
-     *    response=422,
-     *    description="Wrong credentials response",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
-     *        )
-     *     )
-     * ),
-     *  @OA\Response(
-     *         response=203,
-     *         description="OK",
-     *         @OA\JsonContent(
-     *             @OA\Property(
+     *      path="/Brand",
+     *      summary="delete the row of the brands that is determined",
+     *      description="update name and slug by admin",
+     *      tags={"brands"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="delete brands name and slug",
+     *          @OA\JsonContent(
+     *              required={"id"},
+     *              @OA\Property(property="id", type="string", format="id", example="1"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not_Found",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Sorry, your data not supported.")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="delete",
+     *          @OA\JsonContent(
+     *              @OA\Property(
      *                     property="message",
      *                     type="string"
-     *                  ),
-     *         )
-     *     ),
+     *              ),
+     *          )
+     *      ),
+     * ),
+     *
      *
      */
     public function delete(Request  $request){
