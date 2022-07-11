341b0a8f150b67a25d413109318705cdb23cdce1
za1377 <zahraanvari216@gmail.com>
2022-07-10 23:54:29 -0600

    edit the commend of swagger in snactum controller

--- app/Http/Controllers/SanctumController.php
+++ app/Http/Controllers/SanctumController.php
@@ -11,84 +11,84 @@ use Illuminate\Support\Facades\Hash;
 class SanctumController extends Controller {
     /**
      * @OA\Post(
-     * path="/login",
-     * summary="Sign in",
-     * description="Login by email, password",
-     * operationId="authLogin",
-     * tags={"auth"},
-     * @OA\RequestBody(
-     *    required=true,
-     *    description="Pass user credentials",
-     *    @OA\JsonContent(
-     *       required={"email","password"},
-     *       @OA\Property(property="email", type="string", format="email", example="feest.delphine@example.net"),
-     *       @OA\Property(property="password", type="string", format="password", example="password"),
-     *    ),
-     * ),
-     * @OA\Response(
-     *    response=422,
-     *    description="Wrong credentials response",
-     *    @OA\JsonContent(
-     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
-     *        )
-     *     )
-     * ),
-     * @OA\Response(
-     *         response=200,
-     *         description="OK",
-     *         @OA\JsonContent(
-     *             @OA\Property(
-     *                     property="token",
-     *                     type="string"
+     *      path="/login",
+     *      summary="Sign in",
+     *      description="Login by email, password",
+     *      operationId="authLogin",
+     *      tags={"auth"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Pass user credentials",
+     *          @OA\JsonContent(
+     *              required={"email","password"},
+     *              @OA\Property(property="email", type="string", format="email", example="feest.delphine@example.net"),
+     *              @OA\Property(property="password", type="string", format="password", example="password"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Wrong credentials response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *              response=200,
+     *              description="OK",
+     *              @OA\JsonContent(
+     *                  @OA\Property(
+     *                          property="token",
+     *                          type="string"
      *                  )
+     *              )
+     *      )
+     * ),
      *
-     *         )
-     *     )
      */
-    // public function logIn(Request $request){
+    public function logIn(Request $request){
 
-    //     $validator = Validator::make($request->all(), [
-    //         'email' => 'required|string|email',
-    //         'password' => 'required',
-    //     ]);
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required',
+        ]);
 
-    //     $validated = $validator->validated();
+        $validated = $validator->validated();
 
-    //     $user = Admin::where('email', $request->email )->first();
+        $user = Admin::where('email', $request->email )->first();
 
-    //     if($user && Hash::check($validated['password'], $user->password)){
-    //         return response()->json([
-    //             "token" =>$user->createToken('token')->plainTextToken
-    //         ], 200);
-    //     }
-    // }
+        if($user && Hash::check($validated['password'], $user->password)){
+            return response()->json([
+                "token" =>$user->createToken('token')->plainTextToken
+            ], 200);
+        }
+    }
 
     /**
      * @OA\Get(
-     * path="/home",
-     * summary="home",
-     * description="Lsdsdsdsd",
-     * operationId="sdsdsd",
-     * tags={"home"},
-     * @OA\Response(
-     *         response=200,
-     *         description="OK",
-     *         @OA\JsonContent(
-     *             @OA\Property(
-     *                     property="message",
-     *                     type="string"
+     *      path="/home",
+     *      summary="home",
+     *      description="Lsdsdsdsd",
+     *      operationId="sdsdsd",
+     *      tags={"home"},
+     *      @OA\Response(
+     *              response=200,
+     *              description="OK",
+     *              @OA\JsonContent(
+     *                  @OA\Property(
+     *                          property="message",
+     *                          type="string"
      *                  )
-     *         )
-     *     )
+     *              )
+     *      )
      * )
      */
-    // public function home()
-    // {
-    //     try {
-    //         return response()->json(["message" => "Karim"], 200);
-    //     } catch (\Exception $e) {
-    //         return response()->json(["message" => $e->getMessage()], 200);
-    //     }
-    // }
+    public function home()
+    {
+        try {
+            return response()->json(["message" => "Karim"], 200);
+        } catch (\Exception $e) {
+            return response()->json(["message" => $e->getMessage()], 200);
+        }
+    }
 
 }
