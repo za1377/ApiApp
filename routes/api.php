484ec18b20f6975ec8f8b9e957b79983f0989af2
za1377 <zahraanvari216@gmail.com>
2022-07-19 01:49:57 -0600

    add sanctum auth to all url

--- routes/api.php
+++ routes/api.php
@@ -15,60 +15,58 @@ use App\Http\Controllers\SanctumController;
 |
 */
 
-
-Route::get('/home', 'App\Http\Controllers\SanctumController@home')->middleware('auth:sanctum');
-
-
 Route::post('/login', 'App\Http\Controllers\SanctumController@logIn');
 
-Route::controller('App\Http\Controllers\BrandsController')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::get('/Brands', 'show');
-    Route::post('/Brand', 'insert');
-    Route::put('/Brand', 'update');
-    Route::delete('/Brand', 'delete');
+    Route::controller('App\Http\Controllers\BrandsController')->group(function () {
+        Route::get('/Brands', 'show');
+        Route::post('/Brand', 'insert');
+        Route::put('/Brand', 'update');
+        Route::delete('/Brand', 'delete');
+
+    });
+
+    Route::controller('App\Http\Controllers\CategoriesController')->group(function () {
+
+        Route::get('/Categories', 'show');
+        Route::post('/Category', 'insert');
+        Route::put('/Category', 'update');
+        Route::delete('/Category', 'delete');
+
+    });
+
+    Route::controller('App\Http\Controllers\AttributeCategoriesController')->group(function () {
+
+        Route::get('/Category/Attributes', 'show');
+        Route::post('/Category/Attribute', 'insert');
+        Route::put('/Category/Attribute', 'update');
+        Route::delete('/Category/Attribute', 'delete');
+    });
+
+    Route::controller('App\Http\Controllers\AttributeController')->group(function () {
+
+        Route::get('/Attributes', 'show');
+        Route::post('/Attribute', 'insert');
+        Route::put('/Attribute', 'update');
+        Route::delete('/Attribute', 'delete');
+    });
+
+    Route::controller('App\Http\Controllers\AttributeTypeController')->group(function () {
+
+        Route::get('/Attribute/Types', 'show');
+        Route::post('/Attribute/Type', 'insert');
+        Route::put('/Attribute/Type', 'update');
+
+    });
+
+    Route::controller('App\Http\Controllers\AttributeTypeController')->group(function () {
+
+        Route::get('/Attribute/Values', 'show');
+
+    });
 
 });
 
-Route::controller('App\Http\Controllers\CategoriesController')->group(function () {
 
-    Route::get('/Categories', 'show');
-    Route::post('/Category', 'insert');
-    Route::put('/Category', 'update');
-    Route::delete('/Category', 'delete');
 
-});
-
-Route::controller('App\Http\Controllers\AttributeCategoriesController')->group(function () {
-
-    Route::get('/Category/Attributes', 'show');
-    Route::post('/Category/Attribute', 'insert');
-    Route::put('/Category/Attribute', 'update');
-    Route::delete('/Category/Attribute', 'delete');
-});
-
-Route::controller('App\Http\Controllers\AttributeController')->group(function () {
-
-    Route::get('/Attributes', 'show');
-    Route::post('/Attribute', 'insert');
-    Route::put('/Attribute', 'update');
-    Route::delete('/Attribute', 'delete');
-});
-
-Route::controller('App\Http\Controllers\AttributeTypeController')->group(function () {
-
-    Route::get('/Attribute/Types', 'show');
-    Route::post('/Attribute/Type', 'insert');
-    Route::put('/Attribute/Type', 'update');
-
-});
-
-Route::controller('App\Http\Controllers\AttributeTypeController')->group(function () {
-
-    Route::get('/Attribute/Values', 'show');
-
-});
-
-// Route::middleware('Ensure:sanctum')->get('/', function () {
-//     return response()->json(['name' => "home"]);
-// });
