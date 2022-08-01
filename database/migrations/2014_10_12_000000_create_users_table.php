81fbda71224097b3971167f898144cef2a19a5cc
za1377 <zahraanvari216@gmail.com>
2022-07-31 22:41:17 -0600

    edit the variable of user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigInteger('id' ,true , true);
             $table->string('name');
-            $table->string('phone')->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
