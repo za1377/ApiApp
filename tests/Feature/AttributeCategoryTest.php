148b5fc12c5e6f9d8994b2c8c38b2f2e6c2d6d8b
za1377 <zahraanvari216@gmail.com>
2022-08-27 01:28:37 -0600

    AttributeCategory test

---
+++ tests/Feature/AttributeCategoryTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AttributeCategoryTest extends TestCase
+{
+        /**
+     * A test for showing AttributeCategory.
+     *
+     * @return void
+     */
+    public function testShow()
+    {
+        $this->withoutMiddleware();
+        $response = $this->getJson('api/Category/Attribute/Attributes');
+
+        if($response->getStatusCode() == 404) {
+            $response->assertStatus(404);
+        } else {
+            $response->assertStatus(200);
+        }
+    }
+
+    /**
+     * A test for inserting AttributeCategory.
+     *
+     * @return void
+     */
+    public function testInsert()
+    {
+        $this->withoutMiddleware();
+        $response = $this->postJson('api/Category/Attribute' , ['name' => 'AttributeCategory' , 'slug' => '/AttributeCategory']);
+
+        if($response->getStatusCode() == 422) {
+            $response->assertStatus(422);
+        }else if($response->getStatusCode() == 400){
+            $response->assertStatus(400);
+        }else {
+            $response->assertStatus(201);
+        }
+    }
+
+    /**
+     * A test for updating AttributeCategory.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $this->withoutMiddleware();
+        $response = $this->putJson('api/Category/Attribute' ,
+            ['name' => 'AttributeCategory' ,
+            'slug' => '/AttributeCategory',
+            'id' => 1]);
+
+            if($response->getStatusCode() == 422) {
+                $response->assertStatus(422);
+            }else if($response->getStatusCode() == 400){
+                $response->assertStatus(400);
+            }else {
+                $response->assertStatus(200);
+            }
+    }
+
+    /**
+     * A test for deleting AttributeCategory.
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->withoutMiddleware();
+        $response = $this->deleteJson('api/Category/Attribute' , ['id' => 2]);
+
+        if($response->getStatusCode() == 404){
+            $response->assertStatus(404);
+        }else {
+            $response->assertStatus(200);
+        }
+    }
+}
